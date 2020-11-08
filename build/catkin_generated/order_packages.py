3e4f5dd58fa521bf455f803e7bcbf9a12274f430
Epsilon8854 <whitegoon43@gmail.com>
2020-11-08 17:00:53 +0900

    odom + rf + control change

--- build/catkin_generated/order_packages.py
+++ build/catkin_generated/order_packages.py
@@ -2,4 +2,4 @@
 source_root_dir = '/home/nvidia/autoCar_ws/src'
 whitelisted_packages = ''.split(';') if '' != '' else []
 blacklisted_packages = ''.split(';') if '' != '' else []
-underlay_workspaces = '/home/nvidia/catkin_ws/devel;/opt/ros/melodic'.split(';') if '/home/nvidia/catkin_ws/devel;/opt/ros/melodic' != '' else []
+underlay_workspaces = '/home/nvidia/autoCar_ws/devel;/opt/ros/melodic'.split(';') if '/home/nvidia/autoCar_ws/devel;/opt/ros/melodic' != '' else []
--- build/catkin_generated/setup_cached.sh
+++ build/catkin_generated/setup_cached.sh
@@ -7,7 +7,4 @@
 # new environment variables
 
 # modified environment variables
-export CMAKE_PREFIX_PATH="/home/nvidia/autoCar_ws/devel:$CMAKE_PREFIX_PATH"
 export PWD="/home/nvidia/autoCar_ws/build"
-export ROSLISP_PACKAGE_DIRECTORIES="/home/nvidia/autoCar_ws/devel/share/common-lisp:$ROSLISP_PACKAGE_DIRECTORIES"
-export ROS_PACKAGE_PATH="/home/nvidia/autoCar_ws/src:$ROS_PACKAGE_PATH"
